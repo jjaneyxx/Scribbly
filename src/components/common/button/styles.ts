e55fd488b96692f6d6c5235b1c2c943f4480992f
jjaneyxx <hmyeonge@gmail.com>
2025-04-09 15:45:17 +0900

    feat: 공통 버튼 컴포넌트 구현 (#4)
    - Button.tsx, styles.ts 로 분리하여 구성
    - twMerge 및 clsx 설치 및 적용

---
+++ src/components/common/button/styles.ts
@@ -0,0 +1,10 @@
+export const baseStyles =
+  'flex justify-center items-center box-border hover:opacity-80 cursor-pointer rounded-lg bg-black text-white';
+
+export const disabledStyles = 'cursor-not-allowed opacity-70';
+
+// object
+export const sizeStyles = {
+  sm: 'h-11 px-10 text-base',
+  lg: 'h-13 px-[120px] text-lg',
+};
