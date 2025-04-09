4a6ebdc6a95a4a895a5b4dda90270ed646e2408a
jjaneyxx <hmyeonge@gmail.com>
2025-04-09 19:05:28 +0900

    feat: 헤더 컴포넌트 구현 및 /public 으로 정적 파일 이동 (#6)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import landingLogo from '@/assets/icons/landingLogo.svg';
 import Image from 'next/image';
 import StartButton from '@/components/common/Button/StartButton';
 
@@ -6,7 +5,7 @@ export default function LandingPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-white">
       <main className="flex flex-col justify-center gap-10 p-10 text-2xl text-black">
-        <Image src={landingLogo} alt="logo" className="mx-auto" />
+        <Image src="/icons/logo.svg" alt="logo" className="mx-auto" width={200} height={38} />
         <StartButton />
       </main>
     </div>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,17 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function Header() {
-  return <div>헤더</div>;
+  return (
+    <header className="w-full bg-white py-5">
+      <div className="mx-auto flex max-w-7xl items-center justify-between border px-4">
+        <Link href={'/posts'}>
+          <Image src="/icons/logo.svg" alt="logo" width={133} height={24} />
+        </Link>
+        <Link href={'/posts/create'}>
+          <Image src="/icons/createButton.svg" alt="create button" width={24} height={24} />
+        </Link>
+      </div>
+    </header>
+  );
 }
