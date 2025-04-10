87b3c4346cd4d57677cd0e37a582fb84f8e61a3a
jjaneyxx <hmyeonge@gmail.com>
2025-04-10 16:52:38 +0900

    design: 글 생성 폼 UI 구현 (#15)

--- src/app/(main)/posts/create/CreatePostForm.tsx
+++ src/app/(main)/posts/create/CreatePostForm.tsx
@@ -5,10 +5,23 @@ import { handleInsertPost } from '@/utils/handleFormSubmit';
 
 export default function CreatePostForm() {
   return (
-    <form action={handleInsertPost}>
-      <input name="title" type="text" placeholder="제목을 작성하세요" />
-      <input name="content" type="text" placeholder="내용을 작성하세요" />
-      <Button type="submit">등록하기</Button>
+    <form action={handleInsertPost} className="flex flex-col">
+      <div className="flex flex-col gap-3">
+        <input
+          name="title"
+          type="text"
+          placeholder="제목을 작성하세요"
+          className="border bg-white px-5 py-4 text-2xl text-black"
+        />
+        <textarea
+          name="content"
+          placeholder="내용을 입력하세요"
+          className="h-[160px] border bg-white px-5 py-4 text-black"
+        />
+      </div>
+      <div className="mx-auto mt-6">
+        <Button type="submit">등록하기</Button>
+      </div>
     </form>
   );
 }
