a51ae61ee788af841889ec6b3e783c6c94179917
jjaneyxx <hmyeonge@gmail.com>
2025-04-12 14:35:25 +0900

    design: add dropdown svg to component (#8)

--- src/components/common/dropdown/index.tsx
+++ src/components/common/dropdown/index.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+
 export default function DropDown() {
-  return <div>DropDown</div>;
+  return (
+    <button className="cursor-pointer">
+      <Image
+        src="/icons/headerKebap.svg"
+        alt="header-kebap"
+        width={0}
+        height={0}
+        style={{ width: '32px', height: 'auto' }}
+      />
+    </button>
+  );
 }
